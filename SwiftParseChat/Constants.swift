092e50cb9004e5613ddcda8e17dd3db72b972120
Jesse Hu <huyouare@gmail.com>
2015-02-20 21:32:47 -0500

    Import constants from NotificationChat

---
+++ SwiftParseChat/Constants.swift
@@ -0,0 +1,70 @@
+//
+//  Constants.swift
+//  SwiftParseChat
+//
+//  Created by Jesse Hu on 2/20/15.
+//  Copyright (c) 2015 Jesse Hu. All rights reserved.
+//
+
+import Foundation
+
+/* TODO: Try struct format for constants */
+struct Constants {
+    struct PF {
+        struct Installation {
+            static let ClassName = "_Installation"
+        }
+    }
+}
+
+//let HEXCOLOR(c) = [UIColor colorWithRed:((c>>24)&0xFF)/255.0 green:((c>>16)&0xFF)/255.0 blue:((c>>8)&0xFF)/255.0 alpha:((c)&0xFF)/255.0]
+
+let DEFAULT_TAB							= 0
+
+let MESSAGE_INVITE						= "Check out NotificationChat. You can download here: https://github.com/relatedcode/NotificationChat"
+
+/* Installation */
+let PF_INSTALLATION_CLASS_NAME			= "_Installation"           //	Class name
+let PF_INSTALLATION_OBJECTID			= "objectId"				//	String
+let PF_INSTALLATION_USER				= "user"					//	Pointer to User Class
+
+/* User */
+let PF_USER_CLASS_NAME					= "_User"                   //	Class name
+let PF_USER_OBJECTID					= "objectId"				//	String
+let PF_USER_USERNAME					= "username"				//	String
+let PF_USER_PASSWORD					= "password"				//	String
+let PF_USER_EMAIL						= "email"                   //	String
+let PF_USER_EMAILCOPY					= "emailCopy"               //	String
+let PF_USER_FULLNAME					= "fullname"				//	String
+let PF_USER_FULLNAME_LOWER				= "fullname_lower"          //	String
+let PF_USER_FACEBOOKID					= "facebookId"              //	String
+let PF_USER_PICTURE						= "picture"                 //	File
+let PF_USER_THUMBNAIL					= "thumbnail"               //	File
+
+/* Chat */
+let PF_CHAT_CLASS_NAME					= "Chat"					//	Class name
+let PF_CHAT_USER						= "user"					//	Pointer to User Class
+let PF_CHAT_ROOMID						= "roomId"                  //	String
+let PF_CHAT_TEXT						= "text"					//	String
+let PF_CHAT_PICTURE						= "picture"                 //	File
+let PF_CHAT_VIDEO						= "video"                   //	File
+let PF_CHAT_CREATEDAT					= "createdAt"               //	Date
+
+/* Chatrooms */
+let PF_CHATROOMS_CLASS_NAME				= "ChatRooms"               //	Class name
+let PF_CHATROOMS_NAME					= "name"					//	String
+
+/* Messages*/
+let PF_MESSAGES_CLASS_NAME				= "Messages"				//	Class name
+let PF_MESSAGES_USER					= "user"					//	Pointer to User Class
+let PF_MESSAGES_ROOMID					= "roomId"                  //	String
+let PF_MESSAGES_DESCRIPTION				= "description"             //	String
+let PF_MESSAGES_LASTUSER				= "lastUser"				//	Pointer to User Class
+let PF_MESSAGES_LASTMESSAGE				= "lastMessage"             //	String
+let PF_MESSAGES_COUNTER					= "counter"                 //	Number
+let PF_MESSAGES_UPDATEDACTION			= "updatedAction"           //	Date
+
+/* Notification */
+let NOTIFICATION_APP_STARTED			= "NCAppStarted"
+let NOTIFICATION_USER_LOGGED_IN			= "NCUserLoggedIn"
+let NOTIFICATION_USER_LOGGED_OUT		= "NCUserLoggedOut"
