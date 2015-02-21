ab89c93c4071f4697e13c8dcca347cce9a773cd8
Jesse Hu <huyouare@gmail.com>
2015-02-20 20:20:48 -0500

    Modify Table View methods

--- SwiftParseChat/GroupViewController.swift
+++ SwiftParseChat/GroupViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class GroupViewController: UITableViewController {
+class GroupViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate, UIAlertViewDelegate {
+    
+    var chatrooms = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Currentuser login
+        self.loadChatRooms()
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,5 +25,43 @@ class GroupViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func loadChatRooms() {
+        //PFQuery
+        //Add objects from array
+    }
     
+    @IBAction func newButtonPressed(sender: UIBarButtonItem) {
+        self.actionNew()
+    }
+    
+    func actionNew() {
+        var alert = UIAlertView(title: "Please enter a name for your group", message: "", delegate: self, cancelButtonTitle: "Cancel", otherButtonTitles: "OK")
+        alert.alertViewStyle = UIAlertViewStyle.PlainTextInput
+        alert.show()
+    }
+    
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        if buttonIndex != alertView.cancelButtonIndex {
+            var textField = alertView.textFieldAtIndex(0);
+            if let text = textField!.text {
+                if countElements(text) > 0 {
+                    // Save PFObject
+                }
+            }
+        }
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatrooms.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 }
