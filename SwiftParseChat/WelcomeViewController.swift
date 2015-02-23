1a99986fbea2d7f4a8d28fdb0f898eda44e2062f
Jesse Hu <huyouare@gmail.com>
2015-02-22 23:12:24 -0500

    Finish implementing Facebook Login

--- SwiftParseChat/WelcomeViewController.swift
+++ SwiftParseChat/WelcomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class WelcomeViewController: UITableViewController {
     
@@ -18,6 +19,99 @@ class WelcomeViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func facebookLogin(sender: UIButton) {
+        ProgressHUD.show("Signing in...", interaction: false)
+        PFFacebookUtils.logInWithPermissions(["public_profile", "email", "user_friends"], block: { (user: PFUser!, error: NSError!) -> Void in
+            if user != nil {
+                if user[PF_USER_FACEBOOKID] == nil {
+                    self.requestFacebook(user)
+                } else {
+                    self.userLoggedIn(user)
+                }
+            } else {
+                if let info = error.userInfo {
+                    ProgressHUD.showError(info["error"] as String)
+                }
+            }
+        })
+    }
+    
+    func requestFacebook(user: PFUser) {
+        var request = FBRequest.requestForMe()
+        request.startWithCompletionHandler { (connection: FBRequestConnection!, result: AnyObject!, error: NSError!) -> Void in
+            if error == nil {
+                var userData = result as [String: AnyObject]!
+                self.processFacebook(user, userData: userData)
+            } else {
+                PFUser.logOut()
+                ProgressHUD.showError("Failed to fetch Facebook user data")
+            }
+        }
+    }
+    
+    func processFacebook(user: PFUser, userData: [String: AnyObject]) {
+        let facebookUserId = userData["id"] as String
+        var link = "http://graph.facebook.com/\(facebookUserId)/picture"
+        let url = NSURL(string: link)
+        var request = NSURLRequest(URL: url!)
+        let params = ["height": "200", "width": "200", "type": "square"]
+        Alamofire.request(.GET, link, parameters: params).response() {
+            (request, response, data, error) in
+            
+            if error == nil {
+                var image = UIImage(data: data! as NSData)!
+                
+                if image.size.width > 140 {
+                    image = Utilities.resizeImage(image, width: 140, height: 140)
+                }
+                var filePicture = PFFile(name: "picture.jpg", data: UIImageJPEGRepresentation(image, 0.6))
+                filePicture.saveInBackgroundWithBlock({ (success: Bool, error: NSError!) -> Void in
+                    if error != nil {
+                        ProgressHUD.showError("Error saving photo")
+                    }
+                })
+                
+                if image.size.width > 30 {
+                    image = Utilities.resizeImage(image, width: 30, height: 30)
+                }
+                var fileThumbnail = PFFile(name: "thumbnail.jpg", data: UIImageJPEGRepresentation(image, 0.6))
+                fileThumbnail.saveInBackgroundWithBlock({ (success: Bool, error: NSError!) -> Void in
+                    if error != nil {
+                        ProgressHUD.showError("Error saving thumbnail")
+                    }
+                })
+                
+                user[PF_USER_EMAILCOPY] = userData["email"]
+                user[PF_USER_FULLNAME] = userData["name"]
+                user[PF_USER_FULLNAME_LOWER] = (userData["name"] as String).lowercaseString
+                user[PF_USER_FACEBOOKID] = userData["id"]
+                user[PF_USER_PICTURE] = filePicture
+                user[PF_USER_THUMBNAIL] = fileThumbnail
+                user.saveInBackgroundWithBlock({ (succeeded: Bool, error: NSError!) -> Void in
+                    if error == nil {
+                        self.userLoggedIn(user)
+                    } else {
+                        PFUser.logOut()
+                        if let info = error!.userInfo {
+                            ProgressHUD.showError(info["error"] as String)
+                        }
+                    }
+                })
+            } else {
+                PFUser.logOut()
+                if let info = error!.userInfo {
+                    ProgressHUD.showError(info["Facebook photo fetch error"] as String)
+                }
+            }
+        }
+    }
+    
+    func userLoggedIn(user: PFUser) {
+        PushNotication.parsePushUserAssign()
+        ProgressHUD.showSuccess("Welcome back, \(user[PF_USER_FULLNAME])!")
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
 }
