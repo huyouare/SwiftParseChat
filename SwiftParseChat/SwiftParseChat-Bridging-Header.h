f77d8dea14cf3134dcee6ead084ee0185daccac4
Jesse Hu <huyouare@gmail.com>
2015-02-28 20:24:51 -0500

    Add ParseUI to dependecnies

--- SwiftParseChat/SwiftParseChat-Bridging-Header.h
+++ SwiftParseChat/SwiftParseChat-Bridging-Header.h
@@ -7,6 +7,7 @@
 //
 
 #import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
 #import <ParseFacebookUtils/PFFacebookUtils.h>
 #import <MBProgressHUD/MBProgressHUD.h>
 #import <ProgressHUD/ProgressHUD.h>
