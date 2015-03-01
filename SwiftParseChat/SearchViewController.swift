6d6de98303eca4fde4fef003312748a3ba10f276
Jesse Hu <huyouare@gmail.com>
2015-03-01 02:00:08 -0500

    Begin work on SearchVC

---
+++ SwiftParseChat/SearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.swift
+//  SwiftParseChat
+//
+//  Created by Jesse Hu on 3/1/15.
+//  Copyright (c) 2015 Jesse Hu. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+    }
+}
