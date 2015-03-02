2c43f80f1ff1b83d33908476046cfa4703ad82e4
Jesse Hu <huyouare@gmail.com>
2015-03-01 23:41:59 -0500

    Implement contact loading and filtering for Parse users

--- SwiftParseChat/PrivateViewController.swift
+++ SwiftParseChat/PrivateViewController.swift
@@ -12,8 +12,12 @@ import MessageUI
 
 class PrivateViewController: UITableViewController {
     
-    var users1 = [[String:String]]()
-    var users2 = [[String:String]]()
+    var users1 = [APContact]()
+    var users2 = [PFUser]()
+    
+    // activity: UIActivityIndicatorView
+    
+    let addressBook = APAddressBook()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,15 +32,28 @@ class PrivateViewController: UITableViewController {
     
     override func viewDidAppear(animated: Bool) {
         if PFUser.currentUser() != nil {
-            let addressBook: ABAddressBook = ABAddressBookCreateWithOptions(nil, nil).takeRetainedValue()
-            ABAddressBookRequestAccessWithCompletion(addressBook, { (granted: Bool, error: CFError!) -> Void in
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    if granted {
-//                        self.loadAddressBook()
+            // Load address book
+            self.addressBook.fieldsMask = APContactField.Default | APContactField.Emails
+
+            self.addressBook.sortDescriptors = [NSSortDescriptor(key: "firstName", ascending: true), NSSortDescriptor(key: "lastName", ascending: true)]
+            // TODO: Maybe mask for emails
+            //self.addressBook.filterBlock = {(contact: APContact!) -> Bool in
+            //    return contact.emails.count > 0
+            //}
+            self.addressBook.loadContacts({ (contacts: [AnyObject]!, error: NSError!) -> Void in
+                // self.activity.stopAnimating()
+                if contacts != nil {
+                    for contact in contacts as [APContact]! {
+                        self.users1.append(contact)
                     }
-                })
+                    self.loadUsers()
+                } else if error != nil {
+                    ProgressHUD.showError("Error loading contacts")
+                    println(error)
+                }
             })
-        } else {
+        }
+        else {
             Utilities.loginUser(self)
         }
     }
@@ -49,79 +66,53 @@ class PrivateViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-//    func loadAddressBook() {
-//        if ABAddressBookGetAuthorizationStatus() == ABAuthorizationStatus.Authorized {
-//
-//            let addressBook: ABAddressBook = ABAddressBookCreateWithOptions(nil, nil).takeRetainedValue()
-//            let sourceBook: ABRecordRef = ABAddressBookCopyDefaultSource(addressBook).takeRetainedValue()
-//            let allPeople = ABAddressBookCopyArrayOfAllPeopleInSourceWithSortOrdering(addressBook, sourceBook, ABPersonGetSortOrdering())
-//            let personCount = CFArrayGetCount(allPeople.takeRetainedValue())
-//            
-//            users1.removeAll(keepCapacity: false)
-//            
-//            for i in 0..<personCount {
-//                var tmp: ABMultiValueRef!
-//                let person: ABRecordRef = CFArrayGetValueAtIndex(allPeople.takeRetainedValue(), i as CFIndex).memory as ABRecor
-//                
-//                var first = ""
-//                tmp = ABRecordCopyValue(person, kABPersonFirstNameProperty)
-//                if tmp != nil {
-//                    first = "\(tmp)"
-//                }
-//                
-//                var last = ""
-//                tmp = ABRecordCopyValue(person, kABPersonLastNameProperty)
-//                if tmp != nil {
-//                    last = "\(tmp)"
-//                }
-//                
-//                let emails = [String]()
-//                
-//                let multi1 = ABRecordCopyValue(person, kABPersonEmailProperty)
-//                for j in 0..<ABMultiValueGetCount(multi1) {
-//                    tmp = ABMultiValueCopyValueAtIndex(multi1, j)
-//                    if tmp != nil {
-//                        emails.append("\(tmp)")
-//                    }
-//                }
-//                
-//                let phones = [String]()
-//                
-//                let multi2 = ABRecordCopyValue(person, kABPersonPhoneProperty)
-//                for j in 0..<ABMultiValueGetCount(multi2) {
-//                    tmp = ABMultiValueCopyValueAtIndex(multi2, j)
-//                    if tmp != nil {
-//                        phones.append("\(tmp)")
-//                    }
-//                }
-//                
-//                let name = "\(first) \(last)"
-//                users1.append(["name": name, "emails": emails, "phones": phones])
-//            }
-//            self.loadUsers()
-//        }
-//    }
-//    
-//    func loadUsers() {
-//        let emails = [String]()
-//        for user in users1 {
-//            emails += user["emails"]
-//        }
-//        
-//        var user = PFUser.currentUser()
-//        
-//        var query = PFQuery(className: PF_USER_CLASS_NAME)
-//        query.whereKey(PF_USER_OBJECTID, notEqualTo: user.objectId)
-//        query.whereKey(PF_USER_EMAILCOPY, containedIn: emails)
-//        query.orderByAscending(PF_USER_FULLNAME)
-//        query.limit = 1000
-//        query.findObjectsInBackgroundWithBlock { (objects: [AnyObject]!, error: NSError!) -> Void in
-//            if error == nil {
-//                users2.removeAll(keepCapacity: false)
-//                for user in objects as [PFUser]! {
-//                    
-//                }
-//            }
-//        }
-//    }
+    func loadUsers() {
+        var emails = [String]()
+        
+        for user in users1 {
+            if let userEmails = user.emails {
+                emails += userEmails as [String]
+            }
+        }
+        
+        println(emails)
+        
+        var user = PFUser.currentUser()
+        
+        var query = PFQuery(className: PF_USER_CLASS_NAME)
+        query.whereKey(PF_USER_OBJECTID, notEqualTo: user.objectId)
+        query.whereKey(PF_USER_EMAILCOPY, containedIn: emails)
+        query.orderByAscending(PF_USER_FULLNAME)
+        query.limit = 1000
+        query.findObjectsInBackgroundWithBlock { (objects: [AnyObject]!, error: NSError!) -> Void in
+            if error == nil {
+                self.users2.removeAll(keepCapacity: false)
+                for user in objects as [PFUser]! {
+                    self.users2.append(user)
+                    self.removeUser(user[PF_USER_EMAILCOPY] as String)
+                }
+                self.tableView.reloadData()
+            } else {
+                ProgressHUD.showError("Network error")
+            }
+        }
+    }
+    
+    func removeUser(removeEmail: String) {
+        var removeUsers = [APContact]()
+        
+        for user in users1 {
+            if let userEmails = user.emails {
+                for email in userEmails as [String] {
+                    if email == removeEmail {
+                        removeUsers.append(user)
+                        break
+                    }
+                }
+            }
+        }
+        
+        self.users1.filter { !contains(removeUsers, $0) }
+    }
+
 }
