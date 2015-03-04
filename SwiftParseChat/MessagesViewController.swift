b72c524d7478ef2a5b3901be1a4c5a32b43e8a08
Jesse Hu <huyouare@gmail.com>
2015-03-04 00:03:02 -0500

    Create new MessagesVC file

--- SwiftParseChat/MessagesViewController.swift
+++ SwiftParseChat/MessagesViewController.swift
@@ -1,18 +1,17 @@
 //
 //  MessagesViewController.swift
-//  SwiftParseChat
+//  
 //
-//  Created by Jesse Hu on 2/20/15.
-//  Copyright (c) 2015 Jesse Hu. All rights reserved.
+//  Created by Jesse Hu on 3/3/15.
+//
 //
 
 import UIKit
 
 class MessagesViewController: UITableViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,9 +19,7 @@ class MessagesViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    class func refreshMessagesView() {
-        
+    override func viewDidAppear(animated: Bool) {
     }
     
 }
-
